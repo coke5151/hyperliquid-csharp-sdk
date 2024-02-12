0b0a46b4668608a1b834407a320ffb3b60459269
coke5151 <houjunqimail@gmail.com>
2024-02-13 00:53:23 +0800

    init

---
+++ src/HyperLiquidCSharpSDK/Consts.cs
@@ -0,0 +1,12 @@
+﻿namespace HyperLiquidCSharpSDK
+{
+    class Consts
+    {
+        public static string MAINNET_API_URL = "https://api.hyperliquid.xyz";
+        public static string TESTNET_API_URL = "https://api.hyperliquid-testnet.xyz";
+        public static string LOCAL_API_URL = "http://localhost:3001";
+        public const double EPSILON = 1e-9;
+        public internal const ushort INF_BPS = 10_001;
+    }
+}
+}
---
+++ src/HyperLiquidCSharpSDK/Req.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace HyperLiquidSDK
+{
+
+}
